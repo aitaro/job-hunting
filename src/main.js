726631b11365f455cc5e1fd51565da2690ba5bc1
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-06-15 12:00:08 +0900

    bootstrapの導入

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import firebase from 'firebase'
 import firebase from "firebase/app"
+import BootstrapVue from 'bootstrap-vue'
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
